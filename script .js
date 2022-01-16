cec810e52830713b58eae50596e4a5f877a8fd28
DinorshoRahmatov <dinorshorahmatov2002@gmail.com>
2022-01-17 00:46:50 +0500

    sass

---
+++ script .js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+  $("#button").click(function () {
+    $(".creslo").css("background-color", "blue").remove;
+  });
+});
---
+++ script.js
@@ -0,0 +1,6 @@
+const clicks = document.getElementById("creslo");
+const buttons = document.getElementById("button");
+
+buttons.addEventListener("click", function () {
+  clicks.innerHTML = this.style.src=//www.google.com/search?q=katinka&sxsrf=AOaemvLk-F0dP0SQYId-TkzDgeN1TyyYqA:1642159417049&source=lnms&tbm=isch&sa=X&sqi=2&ved=2ahUKEwjWr97FkLH1AhW8JEQIHY2QC9kQ_AUoAXoECAMQAw#imgrc=mTaWicW9qYliiM;
+})
